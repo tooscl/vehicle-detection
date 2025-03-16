a0aea0fa1a06c4cf3e4c187106f14b69e383403a
tooscl <ia-babkn@yandex.ru>
2025-03-16 23:27:52 +0300

    Object detection WIP

--- src/main.py
+++ src/main.py
@@ -4,9 +4,13 @@ import time
 
 from capture import get_cap
 from markup import bounds, lines
+from models.yolov8n import yolo8vn
+from cls import CLASS_MAP
 
 def main():
     cap = get_cap() # Инициализация захвата видео
+
+    model = yolo8vn # Инициализация модели
 
     frame_count = 0
     while cap.isOpened():
@@ -14,11 +18,23 @@ def main():
         if not ret:
             break
 
+        # Разметка фрейма
         cv2.polylines(frame, [bounds[0]], isClosed=True, color=bounds[1], thickness=2) # Устанавливаем границу
         for line in lines.values():
             start_point, end_point, color = line
             thickness = 2
             cv2.line(frame, start_point, end_point, color, thickness)
+
+        # Определение объектов
+        results = model(frame)[0] # Детекции
+
+        # Обход детекций
+        for box in results.boxes.data:
+            x1, y1, x2, y2, conf, cls = box
+            #     cls = int(cls)
+
+            # Визуализация боксов
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
 
         # Вывод изображения
         cv2.imshow("Traffic Monitoring", frame)
--- src/markup.py
+++ src/markup.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-
+# RGB -> BGR
 bounds = (np.array([[219, 418], [679, 276], [1231, 515], [729, 966], [429, 979]], np.int32), (15, 245, 76))
 lines = {
-        "to-left-delimiter": ((259, 527), (759, 309), (241, 130, 32)),
-        "to-right-delimiter": ((362, 797), (960, 396), (241, 130, 32)),
-        "left-right-delimiter": ((301, 636), (847, 350), (235, 28, 28)),
-        "to-bottom-delimiter": ((375, 371), (580, 493), (37, 195, 223)),
-        "to-top-delimiter": ((679, 441), (1085, 646), (37, 195, 223)),
-        "bottom-top-delimiter-left": ((580, 493), (1006, 717), (21, 63, 231)),
-        "bottom-top-delimiter-right": ((492, 336), (679, 441), (21, 63, 231)),
+        "to-left-delimiter": ((259, 527), (759, 309), (32, 130, 241)),
+        "to-right-delimiter": ((362, 797), (960, 396), (32, 130, 241)),
+        "left-right-delimiter": ((301, 636), (847, 350), (28, 28, 235)),
+        "to-bottom-delimiter": ((375, 371), (580, 493), (223, 195, 37)),
+        "to-top-delimiter": ((679, 441), (1085, 646), (223, 195, 37)),
+        "bottom-top-delimiter-left": ((580, 493), (1006, 717), (231, 63, 21)),
+        "bottom-top-delimiter-right": ((492, 336), (679, 441), (231, 63, 21)),
     }
